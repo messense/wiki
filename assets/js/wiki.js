8226040ee455c8e9bd1fc9713f4ed1c046a57a07
messense <messense@icloud.com>
2014-09-06 10:57:00 +0800

    Update layout and assets

--- assets/js/wiki.js
+++ assets/js/wiki.js
@@ -1,9 +1,11 @@
 jQuery(document).ready(function($) {
-	$("#charpter ul").tableOfContents(
-		$("#page"),
-		{
-			startLevel: 2,
-			depth: 3
-		}
+	$("#charpter ul").toc(
+        "selectors": "h1,h2,h3",
+        "container": "#post",
+        "smoothScrolling": true,
+        "prefix": "toc",
+        "anchorName": function (i, heading, prefix) {
+            return prefix + i;
+        }
 	);
 });
